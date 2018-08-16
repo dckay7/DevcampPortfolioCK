a5e7856c8fadd6207fa0a04b10b96133d4a49637
Christian Kay <dckay7@gmail.com>
2018-08-15 21:06:18 -0600

    intergrated friendly routes for blog post

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: "portfolio_show"
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-s
+ 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
